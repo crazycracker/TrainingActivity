f53ee4cb934f248239dbbd6280471ceda81accc8
Manchala <vinay.manchala@capgemini.com>
2018-01-16 17:27:31 +0530

    test classes skeleton created

---
+++ src/com/ing/billing/test/BillingServicesTest.java
@@ -0,0 +1,109 @@
+package com.ing.billing.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class BillingServicesTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public final void testInsertCustomer() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testInsertPostPaidAccount() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testUpdatePostPaidAccount() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testInsertMonthlybill() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testInsertPlan() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testDeletePostPaidAccount() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testGetMonthlyBill() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testGetCustomerPostPaidAccountAllBills() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testGetCustomerPostPaidAccounts() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testGetCustomer() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testGetAllCustomers() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testGetAllPlans() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testGetPlan() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testGetCustomerPostPaidAccount() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testGetPlanDetails() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testDeleteCustomer() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
